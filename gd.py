d8bf23e047317f9f946cda35f6de54a5cee01025
Jon Craton <jncraton@gmail.com>
2021-03-25 17:16:08 -0400

    Update project

--- gd.py
+++ gd.py
@@ -9,11 +9,7 @@ def calc_gradient(fn, x, z):
     1.0
     """
 
-    delta_x = .01
-    delta_z = .01
-    grad_x = (fn(x+delta_x,z) - fn(x,z)) / delta_x
-    grad_z = (fn(x,z+delta_z) - fn(x,z)) / delta_z
-    return (grad_x, grad_z)
+    return [0,0]
 
 def find_extreme(fn, x=0, z=0, lr=.1):
     """
@@ -25,15 +21,7 @@ def find_extreme(fn, x=0, z=0, lr=.1):
     0
     """
     
-    grad_x, grad_z = calc_gradient(fn, x, z)
-
-    next_x = x - grad_x * lr
-    next_z = z - grad_z * lr
-    
-    if abs(grad_x) > .01 or abs(grad_z) > .01:
-        return find_extreme(fn, next_x, next_z)
-    else:
-        return (x, z)
+    return [0,0]
 
 if __name__ == '__main__':
     print(find_extreme(lambda x, z: x ** 2 + 3*x + 2*z ** 2 + 7))
